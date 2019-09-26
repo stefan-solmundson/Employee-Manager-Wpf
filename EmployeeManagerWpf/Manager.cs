a49f9e4b9b7fae92a44a1730ecbe76f4b357b4ab
Stefan Solmundson <stefan.solmundson@gmail.com>
2019-09-26 11:01:26 +0800

    Employee manager class added. (Manger.cs)

---
+++ EmployeeManagerWpf/Manager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagerCSharp
+{
+    class Manager : Employee
+    {
+        private List<Employee> employees = new List<Employee>();
+
+        public Manager(int employeeNum, string name) : base(employeeNum, name)
+        {
+        }
+
+        public void AddEmployee(Employee empNew)
+        {
+            employees.Add(empNew);
+        }
+
+        public void RemoveEmployee(Employee existingEmp)
+        {
+            employees.Remove(existingEmp);
+        }
+
+        public override string PrintDetails()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(EmployeeNum + " ");
+            sb.Append(name + " " + "\n");
+            foreach (Employee e in employees)
+            {
+                sb.Append("Employee: " + e.name + "\n");
+            }
+            return sb.ToString();
+        }
+    }
+}
