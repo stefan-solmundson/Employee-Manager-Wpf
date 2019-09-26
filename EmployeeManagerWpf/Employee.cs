8cda9ff562564871cfdaf9532b3f34a727f1fb89
Stefan Solmundson <stefan.solmundson@gmail.com>
2019-09-26 10:57:20 +0800

    .gitignore added, Employee class added

---
+++ EmployeeManagerWpf/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagerCSharp
+{
+    class Employee
+    {
+        private int employeeNum;
+        public int EmployeeNum
+        {
+            get { return employeeNum; }
+            set { employeeNum = value; }
+        }
+
+        public string name { get; set; }
+
+        public Employee(int employeeNum, string name)
+        {
+            this.employeeNum = employeeNum;
+            this.name = name;
+        }
+
+        public virtual string PrintDetails()
+        {
+            return employeeNum + " " + name;
+        }
+    }
+}
